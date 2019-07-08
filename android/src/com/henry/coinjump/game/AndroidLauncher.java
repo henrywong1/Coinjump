4c05b4475d9aa94ed6df59a3e51840643ee379a0
Henry Wong <H_wong14@u.pacific.edu>
2019-07-08 15:47:28 -0700

    Started Android game

---
+++ android/src/com/henry/coinjump/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.henry.coinjump.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.henry.coinjump.game.Coinjump;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Coinjump(), config);
+	}
+}
