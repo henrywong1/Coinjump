26c1003f4ffec5885c7604d53c395611670af50d
Henry Wong <H_wong14@u.pacific.edu>
2019-07-08 20:38:47 -0700

    Added sprite, with movement

--- core/src/com/henry/coinjump/game/Coinjump.java
+++ core/src/com/henry/coinjump/game/Coinjump.java
@@ -6,21 +6,41 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import javax.xml.soap.Text;
+
 public class Coinjump extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
-
+	Texture[] man;
+	int state;
+	int pause = 0;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+		man = new Texture[4];
+		man[0] = new Texture("frame-1.png");
+		man[1] = new Texture("frame-2.png");
+		man[2] = new Texture("frame-3.png");
+		man[3] = new Texture("frame-4.png");
+
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
+		if (pause < 8) {
+			pause++;
+		} else {
+			pause = 0;
+			if (state < 3) {
+				state++;
+			} else {
+				state = 0;
+			}
+		}
+		batch.draw(man[state], Gdx.graphics.getWidth() / 2 - man[state].getWidth() / 2, Gdx.graphics.getHeight()/2);
 		batch.end();
 
 	}
